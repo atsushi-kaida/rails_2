accbfffb43983b2ec84bb27748d44bf3a035d977
atsushi kaida <atsushi@atsushinoMacBook-Air.local>
2022-05-21 22:20:35 +0900

    routes.bug add icon_name to user

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
-  get '/rooms', to: 'rooms#index'
-  get 'rooms/new', to: 'rooms#new'
   get 'rooms/posts', to: 'rooms#show'
-  get 'rooms/:id', to: 'rooms#search'
-
   devise_for :users, controllers: {
     registrations: 'registrations',
     sessions: 'sessions'
   }
+  devise_scope :user do
+    get "/user/:id", to: "users#profile"
+  end
+  resources :rooms
   resources :reservations
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   root to: "home#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_21_121730) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_21_131917) do
   create_table "reservations", force: :cascade do |t|
     t.integer "room_id"
     t.datetime "start_day"
@@ -40,6 +40,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_21_121730) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "icon_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
