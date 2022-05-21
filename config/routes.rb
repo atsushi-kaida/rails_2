1b6e1975806f0f9d2b0c644fe16ae2fd9e9d19a0
atsushi kaida <atsushi@atsushinoMacBook-Air.local>
2022-05-21 16:42:20 +0900

    controller for delete method  and rooms

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get '/rooms', to: 'rooms#index'
+  get 'rooms/new', to: 'rooms#new'
+  get 'rooms/posts', to: 'rooms#show'
+  get 'rooms/:id', to: 'rooms#search'
+
+  devise_for :users,controllers: {
+    registrations: 'registrations',
+    sessions: 'sessions'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
