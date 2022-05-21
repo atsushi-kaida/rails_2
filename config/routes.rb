221586b7f4ac46c7d049d1272086c8ffdc875e0b
atsushi kaida <atsushi@atsushinoMacBook-Air.local>
2022-05-21 21:18:54 +0900

    add name column to user

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,11 @@ Rails.application.routes.draw do
   get 'rooms/posts', to: 'rooms#show'
   get 'rooms/:id', to: 'rooms#search'
 
-  devise_for :users,controllers: {
+  devise_for :users, controllers: {
     registrations: 'registrations',
     sessions: 'sessions'
   }
+  resources :reservations
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
