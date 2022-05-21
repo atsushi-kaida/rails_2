1e00dff4182a52c521e4cf43234c9d25689af7da
atsushi kaida <atsushi@atsushinoMacBook-Air.local>
2022-05-21 19:16:43 +0900

    make views make models  for view  before db migrate

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,5 @@
 class HomeController < ApplicationController
-  before_filter :authenticate_user!#, only: %i(index)
+#before_action :authenticate_user!
   def index
-    if user_signed_in?
-    end
   end
 end
---
+++ db/migrate/20220521101524_create_rooms.rb
@@ -0,0 +1,13 @@
+class CreateRooms < ActiveRecord::Migration[7.0]
+  def change
+    create_table :rooms do |t|
+      t.string :name
+      t.string :image_name
+      t.string :place
+      t.integer :price
+      t.string :intro
+
+      t.timestamps
+    end
+  end
+end
