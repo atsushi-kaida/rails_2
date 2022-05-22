0c4f09b79be8e7a8d26ec5e671dba787edba130c
atsushi kaida <atsushi@atsushinoMacBook-Air.local>
2022-05-22 21:12:45 +0900

    home view created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   get 'rooms/posts', to: 'rooms#show'
+  get '/search', to: 'rooms#search'
+
+  resources :rooms
+  resources :reservations
+
   devise_for :users, controllers: {
     registrations: 'registrations',
     sessions: 'sessions'
@@ -7,8 +12,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get "/user/:id", to: "users#profile"
   end
-  resources :rooms
-  resources :reservations
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   root to: "home#index"
